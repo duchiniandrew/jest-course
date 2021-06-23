f241b88c1e707d131efddeedd8a90951e85353ad
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-06-23 16:47:22 -0300

    criado testes com debugg

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -2,7 +2,42 @@ import { Utils } from "../app/Utils"
 
 describe("Utils test suite", () => {
     test("Firts test", () => {
-        const abc = Utils.toUpperCase('abc')
-        console.log("tests work!!")
+        const result = Utils.toUpperCase('abc')
+        expect(result).toBe('ABC')
+    })
+    test("parse simple URL", () => {
+        const parsedUrl = Utils.parseUrl("http://localhost:8080/login")
+        expect(parsedUrl.href).toBe("http://localhost:8080/login")
+        expect(parsedUrl.port).toBe("8080")
+        expect(parsedUrl.protocol).toBe("http:")
+        expect(parsedUrl.query).toEqual({})
+    })
+    test("parse simple URL", () => {
+        const parsedUrl = Utils.parseUrl("http://localhost:8080/login?user=user&password=pass")
+        const expectQuery = {
+            user: 'user',
+            password: 'pass'
+        }
+        expect(parsedUrl.query).toEqual(expectQuery)
+    })
+    test("test invalid URL", () => {
+        function expectError() {
+            Utils.parseUrl('')
+        }
+        expect(expectError).toThrow('Empty URL')
+    })
+    test("test invalid URL with arrow function", () => {
+        expect(() => {
+            Utils.parseUrl('')
+        }).toThrow('Empty URL')
+    })
+    test.only("test invalid URL with try catch", () => {
+        try {
+            Utils.parseUrl('')
+        }
+        catch (error) {
+            expect(error).toBeInstanceOf(Error)
+            expect(error).toHaveProperty('message', 'Empty URL!')
+        }
     })
 })
