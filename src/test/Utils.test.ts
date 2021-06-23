0a4f0efed8d9c742b14b05086760246b7ebd29d4
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-06-23 10:16:47 -0300

    base do projeto

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,8 @@
+import { Utils } from "../app/Utils"
+
+describe("Utils test suite", () => {
+    test("Firts test", () => {
+        const abc = Utils.toUpperCase('abc')
+        console.log("tests work!!")
+    })
+})
